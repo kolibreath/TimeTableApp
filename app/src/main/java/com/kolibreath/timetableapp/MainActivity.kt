cb50e7064e7025b32773df60fb10fa4524d4c95c
kolibreath <734780178@qq.com>
2021-11-08 20:37:52 +0800

    modify activity_main.xml

--- app/src/main/java/com/kolibreath/timetableapp/MainActivity.kt
+++ app/src/main/java/com/kolibreath/timetableapp/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.kolibreath.timetableapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.Window
+import android.view.animation.TranslateAnimation
 import android.widget.ImageButton
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.kolibreath.timetableapp.schedule.weeklyschedule.ExportDialogFragment
-import com.kolibreath.timetableapp.schedule.weeklyschedule.ImportDialogFragment
-import com.kolibreath.timetableapp.schedule.weeklyschedule.TimeTableFragment
-import com.kolibreath.timetableapp.schedule.weeklyschedule.TimeTableFragmentViewAdapter
+import com.kolibreath.timetableapp.schedule.weeklyschedule.*
 import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
@@ -83,6 +87,50 @@ class MainActivity : AppCompatActivity() {
              tvCurWeek.text = "第${position+1}周"
             }
         })
+
+        // 弹出的选择不能使用Window，因为Window会覆盖Toolbar_layout 影响用户体验
+        // 使用View模拟Window弹出的效果
+        val settingView = LayoutInflater.from(this)
+            .inflate(R.layout.view_time_table_settings, null, false).apply{
+                background = ColorDrawable(Color.WHITE)
+                visibility = View.GONE
+            }
+        val settingViewParams = RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        settingViewParams.topMargin = resources.getDimension(R.dimen.toolbar_height)
+            .toInt()
+
+        val rootView = viewPager.parent as ViewGroup
+        rootView.addView(settingView, settingViewParams)
+
+        var canOpen = true
+
+        val shadeView = findViewById<View>(R.id.shade_view).apply {
+            setOnClickListener {
+                if (visibility == View.VISIBLE) {
+                    settingView.slideUp()
+                    visibility = View.GONE
+                    settingView.visibility = View.GONE
+                    canOpen = true
+                }
+            }
+        }
+
+        ibList.setOnClickListener {
+            canOpen = if(canOpen) {
+                settingView.slideDown()
+                shadeView.visibility = View.VISIBLE
+                settingView.visibility = View.VISIBLE
+                false
+            }else {
+                settingView.slideUp()
+                shadeView.visibility = View.GONE
+                settingView.visibility = View.GONE
+                true
+            }
+        }
     }
 
 
@@ -96,6 +144,25 @@ class MainActivity : AppCompatActivity() {
         touchSlopField.isAccessible = true
         val touchSlop = touchSlopField.get(recyclerView) as Int
         touchSlopField.set(recyclerView, touchSlop * 3)
+    }
+
+    private fun View.slideUp() {
+        slide(-dp2px(this.height).toInt())
+    }
+
+    private fun View.slideDown() {
+        slide(0)
+    }
+
+    private fun View.slide(toY: Int) {
+        val animation: TranslateAnimation = if (toY < 0) {
+            TranslateAnimation(0f, 0f, 0f, toY.toFloat())
+        } else {
+            TranslateAnimation(0f, 0f, -dp2px(this.height), 0f)
+        }
+        animation.duration = 250
+        animation.fillAfter = true
+        this.startAnimation(animation)
     }
 
 }
